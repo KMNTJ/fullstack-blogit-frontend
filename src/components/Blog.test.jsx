0728a6a71f1d977cdebdf4a5cb43c59939cc7438
Tuomas Nurminen <tuomas.nurminen.tietoevry@ahti.cloud>
2024-05-23 12:01:28 +0300

    5.14 blogilistan testit, step2 done

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react'
 import { Blog } from './Blog'
-import { vi, test, expect } from 'vitest'
+import userEvent from '@testing-library/user-event'
 
-test('by default renders blog`s title', () => {
+test('By default renders blog`s title', () => {
   const blog = {
     title: 'blogiaihe',
   }
@@ -21,4 +21,37 @@ test('by default renders blog`s title', () => {
   const element1 = screen.getByText('blogiaihe')
 
   expect(element1).toBeDefined()
+})
+
+test('Once blogs `View` -button has been pressed, renders blog`s title, author, url and likes', async () => {
+  const blog = {
+    title: 'blogiaihe',
+    author: 'kirjoittaja',
+    likes: '15',
+    url: 'linkki',
+    userId: { name: 'lisääjä' },
+  }
+
+  const handleUpdateBlogDisplay = vi.fn()
+  const handleRemovedBlogDisplay = vi.fn()
+
+  render(
+    <Blog
+      blog={blog}
+      handleUpdateBlogDisplay={handleUpdateBlogDisplay}
+      handleRemovedBlogDisplay={handleRemovedBlogDisplay}
+    />
+  )
+
+  const viewButton = screen.getByText('view')
+  const user = userEvent.setup()
+  await user.click(viewButton)
+
+  const element1 = screen.getByText('15', { exact: false })
+  const element2 = screen.getByText('linkki', { exact: false })
+  const element3 = screen.getByText('lisääjä', { exact: false })
+
+  expect(element1).toBeDefined()
+  expect(element2).toBeDefined()
+  expect(element3).toBeDefined()
 })
