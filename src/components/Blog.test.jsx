4e7a50eccfeb498a078ddc09a744eb075085834a
Tuomas Nurminen <tuomas.nurminen.tietoevry@ahti.cloud>
2024-05-23 12:53:44 +0300

    5.15 blogilistan testit, step 3 done

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import { Blog } from './Blog'
 import userEvent from '@testing-library/user-event'
+import { expect } from 'vitest'
 
 test('By default renders blog`s title', () => {
   const blog = {
@@ -55,3 +56,39 @@ test('Once blogs `View` -button has been pressed, renders blog`s title, author, 
   expect(element2).toBeDefined()
   expect(element3).toBeDefined()
 })
+
+test('Pressing like button n times triggers n calls to like handler function', async () => {
+  const blog = {
+    title: 'blogiaihe',
+    author: 'kirjoittaja',
+    likes: '15',
+    url: 'linkki',
+    userId: { name: 'lisääjä' },
+  }
+
+  const handleUpdateBlogDisplay = vi.fn()
+  const handleRemovedBlogDisplay = vi.fn()
+
+  render(
+    <Blog
+      blog={blog}
+      handleUpdateBlogDisplay={handleUpdateBlogDisplay}
+      handleRemovedBlogDisplay={handleRemovedBlogDisplay}
+    />
+  )
+
+  const viewButton = screen.getByText('view')
+  const user = userEvent.setup()
+  await user.click(viewButton)
+
+  const likeButton = screen.getByText('like')
+  await user.click(likeButton)
+  await user.click(likeButton)
+  await user.click(likeButton)
+
+  expect(handleUpdateBlogDisplay.mock.calls).toHaveLength(3)
+
+})
+
+
+
